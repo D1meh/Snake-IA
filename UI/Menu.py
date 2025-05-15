e26155d1bc384a07582581256995d4290e3c0991
D1meh <mehdi.taouil06@gmail.com>
2025-05-15 14:13:05 +0200

    Started UI

--- UI/Menu.py
+++ UI/Menu.py
@@ -1,6 +1,8 @@
 import pygame
+import os
 
 WINDOW_SIZE = 1200, 1000
+BUTTON_SIZE = 300, 150
 
 class Menu:
 
@@ -10,8 +12,18 @@ class Menu:
         pygame.display.set_caption("Learn2Slither")
 
     def run(self):
-        titleFont = pygame.font.SysFont("Arial", 50)
-        buttonsFont = pygame.font.SysFont("Arial", 15)
+        titleFont = pygame.font.Font(os.path.join(
+            os.path.dirname(__file__), "statics/PerfectoCalligraphy.ttf"), 70)
+        buttonsFont = pygame.font.Font(os.path.join(
+            os.path.dirname(__file__), "statics/PressStart2P.ttf"), 20)
+
+        background = pygame.image.load(os.path.join(
+            os.path.dirname(__file__), "statics/purple.jpg"))
+        background = pygame.transform.scale(background, WINDOW_SIZE)
+
+        button = pygame.image.load(os.path.join(
+            os.path.dirname(__file__), "statics/button.png"))
+        button = pygame.transform.scale(button, BUTTON_SIZE)
 
         while True:
 
@@ -19,6 +31,7 @@ class Menu:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
+                    exit()
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     pass
@@ -26,12 +39,28 @@ class Menu:
                 if event.type == pygame.KEYDOWN\
                         and event.key == pygame.K_ESCAPE:
                     pygame.quit()
+                    exit()
 
+            # Reset display
             self.SCREEN.fill("black")
-            title = titleFont.render("Learn2Slither", True, "white")
+            self.SCREEN.blit(background, (0, 0))
+
+            # Title
+            title = titleFont.render("Learn2Slither", True, "gold")
             titleRect = title.get_rect()
-            titleRect.topleft = (450, 100)
+            titleRect.topleft = (425, 100)
             self.SCREEN.blit(title, titleRect)
+
+            # Buttons
+            buttonsText = ["Start", "Statistics","Quit"]
+            for idx, text in enumerate(buttonsText):
+                buttonText = buttonsFont.render(text, True, "purple")
+                buttonTextRect = buttonText.get_rect()
+                buttonTextRect.topleft = (505 + (10 - len(text)) * 10,
+                                          462 + 150 * idx)
+                self.SCREEN.blit(button, (450,
+                                          400 + 150 * idx))
+                self.SCREEN.blit(buttonText, buttonTextRect)
 
             pygame.display.flip()
             self.CLOCK.tick(30)
